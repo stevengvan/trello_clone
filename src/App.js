214362053801a531006cf4e1e2a2afeb59b663da
stevengvan <vansteven17@gmail.com>
2023-01-10 17:19:24 -0800

    able to add lists

--- src/App.js
+++ src/App.js
@@ -5,19 +5,6 @@ import Kanban from "./components/Kanban";
 function App() {
   const [title, setTitle] = useState("Trello Clone");
   const [placeholder, setPlaceholder] = useState(title);
-  const [lists, setLists] = useState([
-    {
-      title: "To Do",
-      items: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "testing 1 - 2 -3",
-        "Hello World!",
-        "Hello World!",
-      ],
-    },
-    { title: "Doing", items: ["TEXT", "Totally not Trello"] },
-    { title: "Done", items: [] },
-  ]);
 
   const handleChangeTitle = (e) => {
     let newTitle = e.target.value;
@@ -44,7 +31,7 @@ function App() {
           onBlur={(e) => handleChangeTitle(e)}
         />
       </div>
-      <Kanban data={lists} />
+      <Kanban />
     </div>
   );
 }
--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -8,8 +8,22 @@ import {
 } from "@fortawesome/fontawesome-svg-core/import.macro";
 import "./Kanban.css";
 
-function Kanban({ data }) {
-  const [lists, setList] = useState(data);
+function Kanban() {
+  const [lists, setLists] = useState([
+    {
+      title: "To Do",
+      items: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "testing 1 - 2 -3",
+        "Hello World!",
+        "Hello World!",
+      ],
+    },
+    { title: "Doing", items: ["TEXT", "Totally not Trello"] },
+    { title: "Done", items: [] },
+  ]);
+  const [listTitle, setListTitle] = useState("");
+  const [toggleAddL, setToggleAddL] = useState(false);
   const [dragging, setDragging] = useState(false);
   const dragItem = useRef();
   const dragNode = useRef();
@@ -27,7 +41,7 @@ function Kanban({ data }) {
   const handleDragEnter = (e, params) => {
     console.log("Entering drag..", params);
     const currItem = dragItem.current;
-    setList((oldList) => {
+    setLists((oldList) => {
       let newList = JSON.parse(JSON.stringify(oldList));
       newList[params.gIndex].items.splice(
         params.iIndex,
@@ -56,6 +70,19 @@ function Kanban({ data }) {
       return "current list-item";
     }
     return "list-item";
+  };
+
+  const addList = (e) => {
+    if (listTitle.length > 0) {
+      let newList = {
+        title: listTitle,
+        items: [],
+      };
+      setLists((oldList) => [...oldList, newList]);
+      setListTitle("");
+      e.target.blur();
+    } else {
+    }
   };
 
   return (
@@ -107,21 +134,41 @@ function Kanban({ data }) {
           </div>
         );
       })}
-      <button className="row add-list-button">
-        <FontAwesomeIcon icon={solid("plus")} size="xl" />
-        <h6 className="add-text">Add another list</h6>
-      </button>
-      <div className="column add-list">
-        <input type={"text"} className="add-input" />
-        <div className="row add-buttons">
-          <h6 className="add-text add-button">Add list</h6>
-          <FontAwesomeIcon
-            icon={solid("xmark")}
-            size="2xl"
-            className="add-cancel"
+      {toggleAddL ? (
+        <div className="column add-list">
+          <input
+            type={"text"}
+            className="add-input"
+            value={listTitle}
+            onChange={(e) => setListTitle(e.target.value)}
+            placeholder="Enter list title..."
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addList(e);
+              }
+            }}
           />
+          <div className="row add-buttons">
+            <h6 className="add-text add-button" onClick={(e) => addList(e)}>
+              Add list
+            </h6>
+            <FontAwesomeIcon
+              icon={solid("xmark")}
+              size="2xl"
+              className="add-cancel"
+              onClick={() => setToggleAddL(false)}
+            />
+          </div>
         </div>
-      </div>
+      ) : (
+        <button
+          className="row add-list-button"
+          onClick={() => setToggleAddL(true)}
+        >
+          <FontAwesomeIcon icon={solid("plus")} size="xl" />
+          <h6 className="add-text">Add another list</h6>
+        </button>
+      )}
     </div>
   );
 }
